94dd58ed437a63ae9ba639ffc0a227c16131f8ed
DigitalHubRocks <dstadh17@gmail.com>
2019-08-02 15:46:34 +0800

    crowdcount

--- shared/crowdcount.py
+++ shared/crowdcount.py
@@ -11,14 +11,14 @@ from openpose import pyopenpose as op
 
 # Flags
 parser = argparse.ArgumentParser()
-parser.add_argument("--image_path", default="../../../examples/media/COCO_val2014_000000000241.jpg", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
+parser.add_argument("--img_path", default="../../../examples/media/COCO_val2014_000000000241.jpg", help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
 args = parser.parse_known_args()
 
 # Custom Params (refer to include/openpose/flags.hpp for more parameters)
 params = dict()
 params["model_folder"] = os.path.join(OPENPOSE_DIR,"models")
-params["face"] = False
-params["hand"] = False
+# params["face"] = True
+# params["hand"] = False
 # params['camera_resolution']="752x440" # Frisbee
 # params['camera_resolution']="2400x1200" # golf_crowd
 params['model_pose'] = "BODY_25"
@@ -26,7 +26,17 @@ params['model_pose'] = "BODY_25"
 # params['model_pose'] = "MPI" # 15 keypoints
 # params['model_pose'] = "MPI_4_layers" # 15 keypoints, less accurate but faster
 # params['net_resolution']="-1x368" # default, best balance, 16*23
-params['net_resolution']="-1x880" # 16*51
+imageToProcess = cv2.imread(args[0].img_path)
+ratio = imageToProcess.shape[1] / float(imageToProcess.shape[0])
+# net_height_mult = 69 #set this
+net_height_mult = 60 #set this
+net_height = net_height_mult * 16
+net_width = net_height*ratio
+net_width = int((net_width // 16 + 1) * 16)
+
+params['net_resolution']='{:0}x{:0}'.format(net_width, net_height)
+print("Net Resolution Set: ", params['net_resolution'])
+# params['net_resolution']="-1x1072" # 16*51
 params['scale_number'] = 1
 params['scale_gap'] = 0.25
 # params['maximize_positives'] = False
@@ -59,18 +69,17 @@ try:
 
     # Process Image
     datum = op.Datum()
-    imageToProcess = cv2.imread(args[0].image_path)
-    print(imageToProcess.shape)
-    resize_factor = 1
-    imageToProcess = cv2.resize(imageToProcess,(int(imageToProcess.shape[1]/resize_factor), int(imageToProcess.shape[0]/resize_factor)))
-    print(imageToProcess.shape)
+    # resize_factor = 1
+    # imageToProcess = cv2.resize(imageToProcess,(int(imageToProcess.shape[1]/resize_factor), int(imageToProcess.shape[0]/resize_factor)))
+    # print(imageToProcess.shape)
     datum.cvInputData = imageToProcess
     opWrapper.emplaceAndPop([datum])
 
     # Display Image
-    print("Body keypoints: \n" + str(datum.poseKeypoints))
-    print(datum.poseKeypoints.shape)
+    # print("Body keypoints: \n" + str(datum.poseKeypoints))
+    print("Num of keypoints predicted: ", datum.poseKeypoints.shape)
     # print("Face keypoints: \n" + str(datum.faceKeypoints))
+    # print(datum.faceKeypoints.shape)
     # print("Left hand keypoints: \n" + str(datum.handKeypoints[0]))
     # print("Right hand keypoints: \n" + str(datum.handKeypoints[1]))
 
--- shared/drawer.py
+++ shared/drawer.py
@@ -1,12 +1,36 @@
 import cv2
+from random import randint 
 
 color = (255,255,0)
 
+font = cv2.FONT_HERSHEY_DUPLEX
+fontScale = 5
+fontColor = (255,255,0)
+fontThickness = 5
 
 def draw_keypoints(frame, people_kps, threshold=0.5):
     # print(frame.shape)
     # count = 0
     for kps in people_kps:
+        ###
+        # EUGENE ANG DID THIS
+        # temp1 = randint(0,255)
+        # temp2 = randint(0,255)
+        # b_min = min(temp1, temp2)
+        # b_max = max(temp1, temp2)
+        # temp1 = randint(0,255)
+        # temp2 = randint(0,255)
+        # g_min = min(temp1, temp2)
+        # g_max = max(temp1, temp2)
+        # temp1 = randint(0,255)
+        # temp2 = randint(0,255)
+        # r_min = min(temp1, temp2)
+        # r_max = max(temp1, temp2)
+        # color = (randint(b_min,b_max), randint(g_min,g_max), randint(r_min,r_max))
+        ###
+        max_rgb = 200
+        color = (randint(0,max_rgb), randint(0,max_rgb), randint(0,max_rgb))
+
         for kp in kps:
             x, y, prob = kp
             if prob < threshold:
@@ -14,4 +38,17 @@ def draw_keypoints(frame, people_kps, threshold=0.5):
                 continue
             # count += 1
             # print(x,y)
-            cv2.circle(frame, (x,y), 2, color, -1)
+            cv2.circle(frame, (x,y), 4, color, -1)
+
+    # text_red = 'N P e s C u t d: {}'.format(len(people_kps))
+    # text_white = ' D e p   o n e'
+    text = 'NDPeeps Counted: {}'.format(len(people_kps))
+    # text_red = ' '.join(text[::2])
+    # print(text_red)
+    # text_white = ' ' + ' '.join(text[1::2])
+    # print(text_white)
+    # cv2.putText(frame, text_red, (20,150), font, fontScale, (0,0,255), fontThickness)
+    cv2.rectangle(frame, (20,25), (20+len(text)*92, 25+30*fontScale), (0,0,255), -1)
+    cv2.putText(frame, text, (20,150), font, fontScale, (255,255,255), fontThickness)
+    # cv2.putText(frame, text, (20,150), font, fontScale, fontColor, fontThickness)
+
